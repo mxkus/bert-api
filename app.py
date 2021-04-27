5ae298483ef7504184c203ec19c67133ee515d72
Maximilian Kusterer <m.kusterer@enbw.com>
2021-04-27 14:17:35 +0200

    reformat

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-import os
-import traceback
-from flask import Flask
-from flask import render_template, json, request, g
+from flask import Flask, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-import datetime
-import pickle
-import logging
 
 app = Flask(__name__)
 limiter = Limiter(
@@ -16,39 +10,30 @@ limiter = Limiter(
 )
 
 
-@app.route('/bert/', methods=['GET', 'POST'])
+@app.route("/bert/", methods=["GET", "POST"])
 @app.before_request
 @limiter.limit("10 per minute")
 def answer():
 
-    from transformers import MobileBertForQuestionAnswering, AutoTokenizer
+    from transformers import AutoTokenizer, MobileBertForQuestionAnswering, pipeline
 
-    modelname = 'csarron/mobilebert-uncased-squad-v2'
+    modelname = "csarron/mobilebert-uncased-squad-v2"
 
     model = MobileBertForQuestionAnswering.from_pretrained(modelname)
     tokenizer = AutoTokenizer.from_pretrained(modelname)
 
-    from transformers import pipeline
-    nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+    nlp = pipeline("question-answering", model=model, tokenizer=tokenizer)
     with open("merkel.txt", "r") as file:
         context = file.read()
 
-    response_bert = nlp({
-        'question': request.json["question"],
-        'context': context
-    })
+    response_bert = nlp({"question": request.json["question"], "context": context})
     response = app.response_class(
-        response=response_bert.get("answer", "No answer available."),
-        status=200,
-        mimetype='application/json'
+        response=response_bert.get("answer", "No answer available."), status=200, mimetype="application/json"
     )
 
     return response
 
 
-
-
 if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port=1025)
-
+    app.run(host="0.0.0.0", port=1025)
